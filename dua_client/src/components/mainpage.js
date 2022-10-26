33dacb409f424cb874045f7ddb5a917140b0f6cb
winansb <winans.ben15@gmail.com>
2022-10-26 18:16:05 -0400

    Learning Interfaces

--- dua_client/src/components/mainpage.js
+++ dua_client/src/components/mainpage.js
@@ -2,11 +2,12 @@ import React from "react";
 
 // bootstrap css import
 import "bootstrap/dist/css/bootstrap.css";
+import "../css/styles.css"; 
 
 export default function MainPage() {
 	return (
 		<div className="container-fluid">
-			<h1>Hello DUA!</h1>
+			<h1 className= "test" >Hello DUA!</h1>
 			<button className="btn btn-primary">Test</button>{' '}
 		</div>
 	);
