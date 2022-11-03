4ab18e6ab4746c7fa4a22c7594d88f182af1c21f
winansb <winans.ben15@gmail.com>
2022-11-02 21:58:42 -0400

    Update trialLandingPage.js
    Fixed a link to navigate to the previous page instead of the welcome page.

--- dua_client/src/components/trialLandingPage.js
+++ dua_client/src/components/trialLandingPage.js
@@ -7,7 +7,7 @@ export default function WaitingPage () {
   return(
       <div className="container-fluid">
       <h1 className= "waiting" >Please wait while we get your test ready</h1>
-      <a href="/">
+      <a href="/testingSetup">
         <button className="btn btn-primary dua-btn" >Return</button>{' '}
       </a>
       </div>
