32386319213ba001207f07b32ec658b05a939f5f
winansb <winans.ben15@gmail.com>
2022-11-21 11:46:55 -0500

    Video Player now re-sizes to best fit available

--- dua_client/src/App.js
+++ dua_client/src/App.js
@@ -5,6 +5,8 @@ import WaitingPage from "./components/trialLandingPage";
 import TestSetupPage from "./components/trialSetup";
 import PlaceHolderPage from "./components/placeHolderPage";
 import AdminPage from "./components/adminPage";
+import VideoPageTwo from "./components/displayVideo";
+import VideoPageOne from "./components/displayVideo";
 
 // Delete later, just for testing
 import StorageTestPage from "./components/storageTest";
@@ -19,7 +21,11 @@ const App = () =>
 				<Route path="/testingSetup" element={<TestSetupPage />} />
 				<Route path="/placeHolder" element={<PlaceHolderPage />} />
 				<Route path="/adminSetup" element={<AdminPage/>} />
+				<Route path="/videoDisplayOne" element={<VideoPageOne/>} />
+				<Route path="/videoDisplayTwo" element={<VideoPageTwo/>} />
+
 				<Route path="/storage" element={<StorageTestPage/> }/>
+
 			</Routes>
 		</div>
 	);
--- dua_client/src/components/displayVideo.js
+++ dua_client/src/components/displayVideo.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
-import video from './Videos.driving_sim_place_holder.mp4'
+import React, { Component, useEffect, useState, useRef} from 'react'
+import "bootstrap/dist/css/bootstrap.css";
+import "../css/styles.css";
+//import '~video-react/dist/video-react.css'
+//import { Player } from 'video-react'; 
+import video from "../Videos/driving_sim_place_holder.mp4"
 
-export default function VideoPage () {
+//possible solution for updating the video src and controlling from a separate page
+//https://www.geeksforgeeks.org/how-to-play-pause-video-or-audio-in-reactjs/
+
+export default function VideoPageOne () {
   return (
-    <div className="App">
-    <p>hello</p>
-    <video width="750" height="500" controls >
-    <source src={video} type="video/mp4"/>
-   </video>
-    </div>
+  	<div className="container-fluid text-center p-0 bg-secondary">
+		<video disablepictureinpicture controls  controlslist = "nodownload noremoteplayback noplaybackrate"  className="mw-100 mh-100" >
+			<source src={video} type="video/mp4"/>
+		</video>
+	</div>
   );
 }
+
+// export function VideoPageTwo () {
+//   return (
+//   	<div className="container-fluid text-center p-0 bg-secondary">
+//   	<video className="mw-100 mh-100">
+//   		<source src={video} type="video/mp4"/>
+// 	</video>
+// 	</div>
+//   );
+// }
